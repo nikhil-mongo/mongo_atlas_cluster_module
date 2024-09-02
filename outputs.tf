5a9c5b3fa0345a40fcd41e9545176fd8dee4b05c
Nikhil Singh <nikhil.singh@mongodb.com>
2024-09-02 14:35:39 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "cluster_id" {
-  value = mongodbatlas_cluster.mongo_atlas_cluster.id
+  value = mongodbatlas_advanced_cluster.mongo_atlas_cluster.id
 }
 
 output "connection_strings" {
-  value = mongodbatlas_cluster.mongo_atlas_cluster.connection_strings
+  value = mongodbatlas_advanced_cluster.mongo_atlas_cluster.connection_strings
 }
