206c8757674c11b0affb3cbe6f0ff0f69ada2ace
Nikhil Singh <nikhil.singh@mongodb.com>
2024-09-02 14:45:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,116 +1,52 @@
 variable "project_id" {
-  description = "The ID of the project in MongoDB Atlas"
+  description = "The ID of the MongoDB Atlas project"
   type        = string
 }
 
-variable "name" {
+variable "cluster_name" {
   description = "The name of the MongoDB Atlas cluster"
   type        = string
 }
 
 variable "cluster_type" {
-  description = "The type of cluster (REPLICASET, SHARDED)"
+  description = "The type of the MongoDB Atlas cluster"
   type        = string
-  default     = "REPLICASET"
+  default     = "SHARDED"
 }
 
-variable "num_shards" {
-  description = "Number of shards in the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "pit_enabled" {
-  description = "Whether PIT backups are enabled"
+variable "backup_enabled" {
+  description = "Enable or disable backup for the cluster"
   type        = bool
-  default     = false
+  default     = true
 }
 
-variable "region_name_1" {
-  description = "Region name for the first region"
-  type        = string
+variable "replication_specs" {
+  description = "List of replication specifications"
+  type = list(object({
+    region_configs = list(object({
+      instance_size = string
+      node_count    = number
+      provider_name = string
+      priority      = number
+      region_name   = string
+    }))
+  }))
 }
-
-variable "electable_nodes_1" {
-  description = "Number of electable nodes in the first region"
-  type        = number
-}
-
-variable "priority_1" {
-  description = "Priority for the first region"
-  type        = number
-}
-
-variable "read_only_nodes_1" {
-  description = "Number of read-only nodes in the first region"
-  type        = number
-  default     = 0
-}
-
-variable "analytics_nodes_1" {
-  description = "Number of analytics nodes in the first region"
-  type        = number
-  default     = 0
-}
-
-# Repeat for other regions
 
 variable "javascript_enabled" {
-  description = "Whether JavaScript is enabled"
+  description = "Enable or disable JavaScript in the cluster"
   type        = bool
   default     = true
 }
 
-variable "minimum_enabled_tls_protocol" {
-  description = "Minimum TLS protocol version"
-  type        = string
-  default     = "TLS1_2"
-}
-
-variable "cloud_backup" {
-  description = "Enable cloud backup"
-  type        = bool
-  default     = true
-}
-
-variable "auto_scaling_disk_gb_enabled" {
-  description = "Enable auto-scaling for disk size"
-  type        = bool
-  default     = true
-}
-
-variable "mongo_db_major_version" {
-  description = "MongoDB major version"
-  type        = string
-  default     = "7.0"
-}
-
-variable "provider_name" {
-  description = "Cloud provider name"
-  type        = string
-  default     = "AWS"
-}
-
-variable "disk_size_gb" {
-  description = "Disk size in GB"
+variable "oplog_size_mb" {
+  description = "Oplog size in MB"
   type        = number
-  default     = 250
+  default     = 991
 }
 
-variable "provider_disk_iops" {
-  description = "Provider disk IOPS"
+variable "sample_refresh_interval_bi_connector" {
+  description = "BI Connector sample refresh interval"
   type        = number
-  default     = 3600
-}
-
-variable "provider_volume_type" {
-  description = "Provider volume type"
-  type        = string
-  default     = "PROVISIONED"
-}
-
-variable "provider_instance_size_name" {
-  description = "Provider instance size name"
-  type        = string
-  default     = "M30"
+  default     = 300
 }
